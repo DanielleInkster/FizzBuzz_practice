8d1d6e84947eba120d27e04ff371bd2e63422e42
DanielleInkster <danielle.inkster@gmail.com>
2019-11-25 18:30:52 +0000

    Setup  First tests for 3

---
+++ FizzBuzz/FizzBuzz/Brain.swift
@@ -0,0 +1,21 @@
+class Brain {
+    
+    
+    func isDivisibleByThree(number: Int) -> Bool{
+        if number % 3 == 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+//
+//  Brain.swift
+//  FizzBuzz
+//
+//  Created by Danielle Inkster on 2019-11-25.
+//  Copyright © 2019 Danielle Inkster. All rights reserved.
+//
+
+import Foundation
